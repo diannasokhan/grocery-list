e9541c270785b83937de19dbc47041071dc81e25
dianna sokhan <diannasokhan@gmail.com>
2019-12-13 22:40:12 -0500

    adding user model and authentication

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,10 +2,12 @@ module.exports = {
     init(app){
         const staticRoutes = require("../routes/static");
         const listRoutes = require("../routes/lists");
-        const itemRoutes = require("../routes/items")
+        const itemRoutes = require("../routes/items");
+        const userRoutes = require("../routes/users");
 
         app.use(staticRoutes);
         app.use(listRoutes);
         app.use(itemRoutes);
+        app.use(userRoutes);
     }
 }
